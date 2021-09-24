faf983a6072b0ca8f39117a3ae9dd11b402dd177
Ayarina <ayat.harastani@gmail.com>
2021-09-24 14:39:12 +0200

    Applicacion

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView texto;
+    private Button boton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        texto = findViewById(R.id.texto_hito);
+        boton = findViewById(R.id.boton_hito);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                texto.setText("patata");
+            }
+        });
+    }
+}
