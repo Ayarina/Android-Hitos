4bf4f706a41828142dd0380a3305b294d0b57338
Ayarina <ayat.harastani@gmail.com>
2021-12-08 12:14:20 +0100

    SignIn function

--- HitoFirebase/app/src/main/java/com/example/hitofirebase/MainActivity.java
+++ HitoFirebase/app/src/main/java/com/example/hitofirebase/MainActivity.java
@@ -1,14 +1,91 @@
 package com.example.hitofirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "EmailPassword";
+    private FirebaseAuth mAuth;
+
+    private Button botonLogin, botonRegister;
+    private EditText correo, contraseña;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        correo = findViewById(R.id.correo_login);
+        contraseña = findViewById(R.id.contraseña_login);
+        botonLogin = findViewById(R.id.boton_login);
+        botonRegister = findViewById(R.id.boton_gotoregister);
+
+        botonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        botonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn(correo.getText().toString(), contraseña.getText().toString());
+            }
+        });
+
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            Toast.makeText(MainActivity.this, "Ha iniciado sesión satisfactoriamente", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        }
+    }
+
+    public void signIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                            Toast.makeText(MainActivity.this, "Ha iniciado sesión satisfactoriamente", Toast.LENGTH_SHORT).show();
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
 }
